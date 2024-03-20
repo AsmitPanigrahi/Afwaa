ffa257cb2a268a8a929d42bee01f3f1399fd5bad
Asmit Panigrahi <128639526+AsmitPanigrahi@users.noreply.github.com>
2024-03-21 04:39:48 +0530

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
+
+// import { StateContextProvider } from './context';
+import App from './App';
+import './index.css';
+import { StateContextProvider } from './context';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <ThirdwebProvider desiredChainId={ChainId.Goerli}> 
+    <Router>
+      <StateContextProvider>
+      <App />
+
+      </StateContextProvider>
+        
+      
+    </Router>
+  </ThirdwebProvider> 
+)
